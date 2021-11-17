e843d1b1c73f0a2bc83b2355949894551b170f67
Tapiens <seba26439@gmail.com>
2021-11-17 18:07:30 -0300

    a

---
+++ app/src/main/java/database/AdminSQLiteOpenHelper.java
@@ -0,0 +1,28 @@
+package database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE pedidos(codigo int primary key, nombre text, monto text)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
